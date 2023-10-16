4da2efa5145dc4604e7a682a79e5b6801b93f215
brcshakil1 <brcshakil72@gmail.com>
2023-10-16 13:00:43 +0600

    deploy on vercel is done

--- index.js
+++ index.js
@@ -94,6 +94,26 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.patch("/user", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updatedDoc = {
+        $set: {
+          loggedInAt: user.loggedInAt,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.deleteOne(query);
+      res.send(user);
+    });
+
     app.post("/user", async (req, res) => {
       const newUser = req.body;
       console.log(newUser);
